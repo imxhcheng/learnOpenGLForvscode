693ab42718615cfc9722359b1ea245276a30baf2
imxhcheng <imxhcheng@outlook.com>
2022-11-09 10:33:52 +0800

    1.添加了光源的着色器代码 2.完善目标显示的逻辑

--- src/08_color/main.cpp
+++ src/08_color/main.cpp
@@ -1,5 +1,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <tool/camera.h>
 #include <tool/shader.h>
 
 #include <glm/glm.hpp>
@@ -8,27 +9,23 @@
 
 #include <iostream>
 
-#include<tool/camera.h>
-
 using namespace std;
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 std::string Shader::dirName;
 
-Camera cam(glm::vec3(0.0f,0.0f,3.0f));
-float lastX = 400;
-float lastY = 300;
+Camera cam(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = WINDOW_WIDTH / 2;
+float lastY = WINDOW_HEIGHT / 2;
 
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
-
-
-glm::vec3 lightPos (1.2f,1.0f,2.0f);
-
-
-
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 int main(int argc, char *argv[])
 {
@@ -55,7 +52,7 @@ int main(int argc, char *argv[])
     return -1;
   }
   // 设置视口
-  glViewport(0, 0, 800, 600);
+  glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
   glEnable(GL_PROGRAM_POINT_SIZE);
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -63,68 +60,140 @@ int main(int argc, char *argv[])
   // 注册窗口变化监听
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-  Shader lightShader("./shader/vertex.glsl", "./shader/fragment.glsl");
-  Shader lightCubeShader("./shader/vertex.glsl", "./shader/fragment.glsl");
+  // 初始化对象的shader
+  Shader lightingShader("./shader/light_vs.glsl", "./shader/light_fs.glsl");
+  Shader ObjectShader("./shader/object_vs.glsl", "./shader/object_fs.glsl");
 
-float vertices[] = {
-        -0.5f, -0.5f, -0.5f, 
-         0.5f, -0.5f, -0.5f,  
-         0.5f,  0.5f, -0.5f,  
-         0.5f,  0.5f, -0.5f,  
-        -0.5f,  0.5f, -0.5f,  
-        -0.5f, -0.5f, -0.5f, 
+  float vertices[] = {
+      -0.5f,
+      -0.5f,
+      -0.5f,
+      0.5f,
+      -0.5f,
+      -0.5f,
+      0.5f,
+      0.5f,
+      -0.5f,
+      0.5f,
+      0.5f,
+      -0.5f,
+      -0.5f,
+      0.5f,
+      -0.5f,
+      -0.5f,
+      -0.5f,
+      -0.5f,
 
-        -0.5f, -0.5f,  0.5f,  
-         0.5f, -0.5f,  0.5f,  
-         0.5f,  0.5f,  0.5f,  
-         0.5f,  0.5f,  0.5f,  
-        -0.5f,  0.5f,  0.5f,  
-        -0.5f, -0.5f,  0.5f,  
+      -0.5f,
+      -0.5f,
+      0.5f,
+      0.5f,
+      -0.5f,
+      0.5f,
+      0.5f,
+      0.5f,
+      0.5f,
+      0.5f,
+      0.5f,
+      0.5f,
+      -0.5f,
+      0.5f,
+      0.5f,
+      -0.5f,
+      -0.5f,
+      0.5f,
 
-        -0.5f,  0.5f,  0.5f,  
-        -0.5f,  0.5f, -0.5f,  
-        -0.5f, -0.5f, -0.5f,  
-        -0.5f, -0.5f, -0.5f,  
-        -0.5f, -0.5f,  0.5f,  
-        -0.5f,  0.5f,  0.5f,  
+      -0.5f,
+      0.5f,
+      0.5f,
+      -0.5f,
+      0.5f,
+      -0.5f,
+      -0.5f,
+      -0.5f,
+      -0.5f,
+      -0.5f,
+      -0.5f,
+      -0.5f,
+      -0.5f,
+      -0.5f,
+      0.5f,
+      -0.5f,
+      0.5f,
+      0.5f,
 
-         0.5f,  0.5f,  0.5f,  
-         0.5f,  0.5f, -0.5f,  
-         0.5f, -0.5f, -0.5f,  
-         0.5f, -0.5f, -0.5f,  
-         0.5f, -0.5f,  0.5f,  
-         0.5f,  0.5f,  0.5f,  
+      0.5f,
+      0.5f,
+      0.5f,
+      0.5f,
+      0.5f,
+      -0.5f,
+      0.5f,
+      -0.5f,
+      -0.5f,
+      0.5f,
+      -0.5f,
+      -0.5f,
+      0.5f,
+      -0.5f,
+      0.5f,
+      0.5f,
+      0.5f,
+      0.5f,
 
-        -0.5f, -0.5f, -0.5f,  
-         0.5f, -0.5f, -0.5f,  
-         0.5f, -0.5f,  0.5f,  
-         0.5f, -0.5f,  0.5f,  
-        -0.5f, -0.5f,  0.5f,  
-        -0.5f, -0.5f, -0.5f,  
+      -0.5f,
+      -0.5f,
+      -0.5f,
+      0.5f,
+      -0.5f,
+      -0.5f,
+      0.5f,
+      -0.5f,
+      0.5f,
+      0.5f,
+      -0.5f,
+      0.5f,
+      -0.5f,
+      -0.5f,
+      0.5f,
+      -0.5f,
+      -0.5f,
+      -0.5f,
 
-        -0.5f,  0.5f, -0.5f,  
-         0.5f,  0.5f, -0.5f,  
-         0.5f,  0.5f,  0.5f,  
-         0.5f,  0.5f,  0.5f,  
-        -0.5f,  0.5f,  0.5f,  
-        -0.5f,  0.5f, -0.5f,  
-    };
+      -0.5f,
+      0.5f,
+      -0.5f,
+      0.5f,
+      0.5f,
+      -0.5f,
+      0.5f,
+      0.5f,
+      0.5f,
+      0.5f,
+      0.5f,
+      0.5f,
+      -0.5f,
+      0.5f,
+      0.5f,
+      -0.5f,
+      0.5f,
+      -0.5f,
+  };
 
-    // world space positions of our cubes
-    glm::vec3 cubePositions[] = {
-        glm::vec3( 0.0f,  0.0f,  0.0f),
-        glm::vec3( 2.0f,  5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3 (2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f,  3.0f, -7.5f),
-        glm::vec3( 1.3f, -2.0f, -2.5f),
-        glm::vec3( 1.5f,  2.0f, -2.5f),
-        glm::vec3( 1.5f,  0.2f, -1.5f),
-        glm::vec3(-1.3f,  1.0f, -1.5f)
-    };
+  // world space positions of our cubes
+  glm::vec3 cubePositions[] = {
+      glm::vec3(0.0f, 0.0f, 0.0f),
+      glm::vec3(2.0f, 5.0f, -15.0f),
+      glm::vec3(-1.5f, -2.2f, -2.5f),
+      glm::vec3(-3.8f, -2.0f, -12.3f),
+      glm::vec3(2.4f, -0.4f, -3.5f),
+      glm::vec3(-1.7f, 3.0f, -7.5f),
+      glm::vec3(1.3f, -2.0f, -2.5f),
+      glm::vec3(1.5f, 2.0f, -2.5f),
+      glm::vec3(1.5f, 0.2f, -1.5f),
+      glm::vec3(-1.3f, 1.0f, -1.5f)};
 
-  // 创建缓冲对象
+  // 创建缓冲对象-需要绘制的目标
   unsigned int VBO, cubeVAO;
   glGenVertexArrays(1, &cubeVAO);
   glGenBuffers(1, &VBO);
@@ -139,31 +208,16 @@ float vertices[] = {
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
 
-
+  // 光源目标
   unsigned int lightCubeVAO;
   glGenVertexArrays(1, &lightCubeVAO);
   glBindVertexArray(lightCubeVAO);
 
-  glBindBuffer(VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
+  // 设置顶点位置属性指针
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
-
-  // pass projection matrix to shader (as projection matrix rarely changes there's no need to do this per frame)
-  // -----------------------------------------------------------------------------------------------------------
-  glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)800 / (float)600, 0.1f, 100.0f);
-  lightShader.setMat4("projection", projection); 
-
-
-  glm::mat4 model = glm::mat4();
-  model = glm::translate(model,lightPos);
-  model = glm::scale(model,glm::vec3(0.2f));
-  lightShader.setMat4("model",model);
-
-
-  glm::mat4 view = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-  view = glm::lookAt(cameraPos, cameraPos+cameraFront, cameraUp);
-  lightShader.setMat4("view",view);
 
 
   while (!glfwWindowShouldClose(window))
@@ -172,23 +226,48 @@ float vertices[] = {
 
     // 渲染指令
     glClearColor(25.0 / 255.0, 25.0 / 255.0, 25.0 / 255.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glEnable(GL_DEPTH_TEST);
 
+    // 1 绘制玩具对象的立方体对象
 
-    lightShader.use();
+    ObjectShader.use();
+    ObjectShader.setVec3("objColor",1.0f,1.0f,0.5f);
+    ObjectShader.setVec3("lightColor",1.0f,1.0f,1.0f);
 
-    //绘制灯的立方体对象
-    glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES,0,36);
+    //view /model /projection
+    glm::mat4 projection = glm::perspective(glm::radians(cam.Zoom), (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, 0.1f, 100.0f);
+    glm::mat4 view = cam.GetViewMatrix();
+    ObjectShader.setMat4("projection", projection);
+    ObjectShader.setMat4("view", view);
 
+    glm::mat4 model = glm::mat4(1.0f);
+    ObjectShader.setMat4("model",model);
 
+    // render 
+    glBindVertexArray(cubeVAO);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+
+    // 2 绘制灯的立方体对象
+    lightingShader.use();
+    lightingShader.setMat4("projection",projection);
+    lightingShader.setMat4("view",view);
+
+    // model更新
+    model = glm::mat4(1.0f);
+    model = glm::translate(model,lightPos);
+    model = glm::scale(model,glm::vec3(0.1f));
+    lightingShader.setMat4("model",model);
+
+    glBindVertexArray(lightCubeVAO);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
 
-  glDeleteVertexArrays(1, &VAO);
+  glDeleteVertexArrays(1, &cubeVAO);
+  glDeleteVertexArrays(1, &lightCubeVAO);
   glDeleteBuffers(1, &VBO);
 
   glfwTerminate();
@@ -207,5 +286,4 @@ void processInput(GLFWwindow *window)
   {
     glfwSetWindowShouldClose(window, true);
   }
-
 }
