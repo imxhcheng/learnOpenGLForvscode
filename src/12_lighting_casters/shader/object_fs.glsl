2771cbc20a392a9bdb21c87cc1e4ae5748dd4397
imxhcheng <imxhcheng@outlook.com>
2023-01-31 20:58:31 +0800

    1、添加投光物的项目代码

---
+++ src/12_lighting_casters/shader/object_fs.glsl
@@ -0,0 +1,52 @@
+#version 330 core
+out vec4 FragColor;
+
+//材质
+struct Material{
+  sampler2D diffuse;
+  sampler2D specular;
+  float shininess;
+};
+
+
+//光照属性
+struct Light{
+  //vec3 position;
+  vec3 direction;
+
+  vec3 ambient;
+  vec3 diffuse;
+  vec3 specular;
+};
+
+in vec3 Normal;
+in vec3 FragPos;
+in vec2 TexCoords;
+
+uniform Material material;
+uniform Light light;
+uniform vec3 viewPos;
+
+
+void main()
+ {
+
+  // 环境光照 == 漫反射光照
+  vec3 ambient = texture(material.diffuse,TexCoords).rgb* light.ambient;
+
+  // 漫反射光照
+  vec3 norm = normalize(Normal);
+  //vec3 lightDir = normalize(light.position - FragPos);
+  vec3 lightDir = normalize(-light.direction);
+  float diff = max(dot(norm,lightDir),0.0);
+  vec3 diffuse = diff*texture(material.diffuse,TexCoords).rgb*light.diffuse;
+
+  // 镜面反射关照
+  vec3 viewDir = normalize(viewPos-FragPos);
+  vec3 reflectDir = reflect(-lightDir,norm);
+  float spec = pow(max(dot(viewDir,reflectDir),0.0),material.shininess);
+  vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;
+
+  vec3 result = (ambient + diffuse + specular);
+  FragColor = vec4(result,1.0);
+}
